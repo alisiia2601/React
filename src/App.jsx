1b1d1f935dc032b66a697704590846de01e4a1b1
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-02-13 17:16:43 +0100

    art

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useState } from 'react'
+import science from './assets/computer.png'
+import science2 from './assets/science2.jpg'
+import styling from './assets/styling.jpg'
 import './App.css'
 
+function getActiveClassName (activeArticle, currentArticle) {
+  if (activeArticle ===  currentArticle) {
+    return 'show-article'
+  } else {
+    return 'hide-article'
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeArticle, setActiveArticle] = useState('First Article')
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <article className={`active ${getActiveClassName(activeArticle, 'First Article')}`} >
+      <h1>About Me</h1>
+        <p>As a front-end developer, I responsible for designing, implementing, and maintaining user. It involves the creation of HTML, CSS, and JavaScript code to define the look, layout, and behavior of a website. My goal is to make websites and web applications fast, interactive, and easy to use for users.</p>
+        <img className='science' src={science} alt="" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
+      </article>
+      <article className={`active ${getActiveClassName(activeArticle, '2nd Article')}`} >
+        <h1>Idea</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
+        <img className='science2' src={science2} alt="" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
+      </article>
+      <article className={`active ${getActiveClassName(activeArticle, '3d Article')}`} >
+        <h1>Styling</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
+        <img className='styling' src={styling} alt="" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
+      </article>
+      <div className='article'>
+        <button onClick={() => {
+          setActiveArticle((a) => {
+            if (a === 'First Article') {
+              return '2nd Article'
+            } else if(a != '2nd Article') {
+              return 'First Article'
+            } else {
+              return '3d Article'
+            }
+          })
+        }}>Switch Article</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
